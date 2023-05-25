3c97327d29331a8eb0440945b000c8fbc0174eec
Karolina <karolina.anna.jesionek@gmail.com>
2023-05-25 12:39:11 +0200

    first version

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const presents = document.querySelectorAll('.present')
+const boxes = document.querySelectorAll('.box')
+const presentsBox = document.querySelector('.presents-box')
+const chosenBox = document.querySelector('.chosen-box')
+const sendBtn = document.querySelector('.send-btn')
+
+presents.forEach(present => {
+	present.addEventListener('dragstart', () => {
+		present.classList.add('is-dragged')
+	})
+
+	present.addEventListener('dragend', () => {
+		present.classList.remove('is-dragged')
+	})
+})
+
+boxes.forEach(box => {
+	box.addEventListener('dragover', e => {
+		e.preventDefault()
+
+		const isDragged = document.querySelector('.is-dragged')
+		box.appendChild(isDragged)
+
+		handlePresents()
+	})
+})
+
+const handlePresents = () => {
+	const chosenPresents = presentsBox.querySelectorAll('.present')
+	if (chosenBox.childElementCount > 2) {
+		chosenPresents.forEach(present => {
+			present.setAttribute('draggable', 'false')
+			present.classList.add('present-disabled')
+		})
+
+		sendBtn.disabled = false
+	} else {
+		chosenPresents.forEach(present => {
+			present.setAttribute('draggable', 'true')
+			present.classList.remove('present-disabled')
+		})
+
+		sendBtn.disabled = true
+	}
+}
